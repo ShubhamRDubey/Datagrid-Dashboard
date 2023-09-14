a9518459f15b975304d86baa52dddd4634fd7400
shubham dubey <shubhamrdubey@outlook.com>
2023-09-14 14:43:25 +0530

    initial ui designed and work on datagrid is in progress

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Datagrid_Dashboard
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+        
+        private bool IsMaximised = false;
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+              if (IsMaximised)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Width = 1080;
+                    this.Height = 100;
+
+                    IsMaximised = false;
+                }
+              else
+                {
+                    this.WindowState = WindowState.Maximized;
+
+                    IsMaximised = true;
+                }
+            }
+        }
+    }
+}
